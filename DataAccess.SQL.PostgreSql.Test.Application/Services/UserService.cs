c368f4455e8b6ec360fe1158b2576efa1b6e93f2
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-06-03 13:39:32 +0400

    adding exception handling in Parallel. foreach scenario

--- DataAccess.SQL.PostgreSql.Test.Application/Services/UserService.cs
+++ DataAccess.SQL.PostgreSql.Test.Application/Services/UserService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using DataAccess.SQL.PostgreSql.Test.Application.Models;
 using DataAccess.SQL.PostgreSql.Test.Application.Repositories.Abstractions;
 using DataAccess.SQL.PostgreSql.Test.Application.Services.Abstractions;
@@ -9,6 +10,12 @@ public class UserService(IUserRepository userRepository) : IUserService
     public async Task AddUsers(List<User> users, CancellationToken cancellationToken)
     {
         // if you are not using transaction, then you can use connection pool to run parallel queries like that
+        /* Parallel.ForEach implementation is designed in way that even if there were multiple exceptions happened
+         it only throws the first one. so even if all the inserts fails, it throws only single exception (The first one).
+         so it is better practise to handle that scenarios by hand. this is the reason why we have written try/catch block
+         in the body of ForEachAsync*/
+
+        ConcurrentBag<Exception> happenedExceptions = [];
 
         await Parallel.ForEachAsync(
             users, 
@@ -19,7 +26,20 @@ public class UserService(IUserRepository userRepository) : IUserService
             },
             async (user , token) =>
             {
-                await userRepository.AddUserAsync(user, token);
+                try
+                {
+                    await userRepository.AddUserAsync(user, token);
+                }
+                catch (Exception ex)
+                {
+                    happenedExceptions.Add(ex);
+                }
             });
+
+        // Logging all the happened exceptions here
+        foreach (Exception happenedException in happenedExceptions)
+        {
+            Console.WriteLine(happenedException.Message);
+        }
     }
 }
