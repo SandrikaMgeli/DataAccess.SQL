3acad30b493a225552215c9a9d3b8d44b96b8ea1
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-06-21 16:56:54 +0400

    Added migraiton ordering

--- DataAccess.SQL.Abstraction/DbOptions.cs
+++ DataAccess.SQL.Abstraction/DbOptions.cs
@@ -1,8 +1,20 @@
+using System.Text.RegularExpressions;
+
 namespace DataAccess.SQL.Abstraction;
 
 public class DbOptions
 {
     private const string MigrationFilePrefix = "migration_";
+
+    /// <summary>
+    /// Matches strings that start with "migration_", followed by a number,
+    /// then an underscore, then any other characters.
+    /// </summary>
+    private const string RegexForValidMigrations = @"^migration_\d+_.*$";
+
+    /// <summary>
+    /// Ordered from oldest to newest
+    /// </summary>
     public List<string> MigrationFilePaths { get; private set; }
     public string ConnectionString { get; private set; }
 
@@ -23,11 +35,19 @@ public class DbOptions
         string[] allSqlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.sql", SearchOption.AllDirectories);
 
         // Filter for files that start with "migration_"
-        var migrationFiles = allSqlFiles.Where(file =>
-        {
-            string fileName = Path.GetFileName(file);
-            return fileName.StartsWith(MigrationFilePrefix, StringComparison.OrdinalIgnoreCase);
-        });
+        IEnumerable<string> migrationFiles = allSqlFiles
+            .Where(file =>
+            {
+                string fileName = Path.GetFileName(file);
+                return fileName.StartsWith(MigrationFilePrefix, StringComparison.OrdinalIgnoreCase) &&
+                       Regex.IsMatch(fileName, RegexForValidMigrations);
+            })
+            .OrderBy(file =>
+            {
+                string[] splitedFileName = Path.GetFileName(file).Split('_');
+                int indexIdentifier = Convert.ToInt32(splitedFileName[1]);
+                return indexIdentifier;
+            });
 
         return migrationFiles;
     }
